baf239bd19c8499ce810c03046e606977f099c26
Ajay Nallanagula <ajay.nallanagulla@ukg.com>
2024-06-21 19:15:41 +0530

    changed first-app

--- src/utility-microapp.d.ts
+++ src/utility-microapp.d.ts
@@ -1,6 +1,5 @@
 // src/utility-microapp.d.ts
 declare module "utility-microapp" {
-    export function publicApiFunction(): string;
-    export function testApiFunction(): string;
-  }
-  
+  export function publicApiFunction(): string;
+  export function testApiFunction(): string;
+}
